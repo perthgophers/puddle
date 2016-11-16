e7c461929e972f5430643040222c7292546b1009
Alex Dunmow <alex.dunmow@blockninja.io>
2016-11-16 20:53:52 +0800

    restarts puddlebot, good for daemonising after manual start

---
+++ responses/restart.go
@@ -0,0 +1,23 @@
+package responses
+
+import (
+	"github.com/perthgophers/puddle/messagerouter"
+	"os"
+	"os/exec"
+)
+
+//Restart restarts Puddlebot.
+func Restart(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) error {
+	w.Write("...Restarting...")
+	stdout.Reset()
+
+	cmd = exec.Command("./run.sh")
+	cmd.Start()
+
+	os.Exit(1)
+	return nil
+}
+
+func init() {
+	Handle("!restart", Restart)
+}
