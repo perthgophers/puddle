d2c365f9213bed3f0d6e6eb344afd699619b83d6
John Nguyen <jnguyen@thefrontiergroup.com.au>
2018-01-03 21:42:09 +0800

    basic cli adaptor

--- responses/portfolio.go
+++ responses/portfolio.go
@@ -13,98 +13,134 @@ import (
 	"github.com/pkg/errors"
 )
 
-func PortfolioWrapper(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) error {
+type PortfolioCommand struct {
+	*cli.App
+}
+
+func NewPortfolioCommand() *PortfolioCommand {
+	return &PortfolioCommand{}
+}
+
+func (app *PortfolioCommand) Initialise(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) {
+
 	cmd := cli.NewApp()
 	cmd.Name = "Portfolio Management"
 	cmd.Usage = "Add, track and manage your crypto portfolio!"
-	cmd.Action = func(c *cli.Context) {
-		fmt.Println(c.Args())
-	}
 	cmd.Commands = []cli.Command{
 		{
-			Name:   "register, r",
-			Usage:  "Register yourself for portfolio tracking",
-			Action: PortfolioRegister,
+			Name:    "register",
+			Aliases: []string{"r"},
+			Usage:   "Register yourself for portfolio tracking",
+			Action:  PortfolioRegister(cr, w),
 		},
 		{
-			Name:  "get, g",
-			Usage: "Get your current portoflio",
-			Action: func(c *cli.Context) {
-				err := Portfolio(cr, w)
-				if err != nil {
-					w.WriteError(err.Error())
-					return
-				}
-			},
+			Name:    "get",
+			Aliases: []string{"g"},
+			Usage:   "Get your current portoflio",
+			Action:  PortfolioGet(cr, w),
 		},
 	}
+
+	app.App = cmd
+}
+
+// PortfolioRegister registers the user for portfolio tracking
+func PortfolioRegister(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) func(c *cli.Context) {
+	return func(c *cli.Context) {
+		fmt.Println("Register")
+		return
+	}
+}
+
+// PortfolioGet gets the current user's portfolio
+func PortfolioGet(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) func(c *cli.Context) {
+	return func(c *cli.Context) {
+		if cr.Username != "nii236" {
+			w.WriteError(errors.New("invalid username " + cr.Username).Error())
+			return
+		}
+
+		ethAmt := os.Getenv("ETH")
+		btcAmt := os.Getenv("BTC")
+		bchAmt := os.Getenv("BCH")
+
+		if ethAmt == "" || btcAmt == "" || bchAmt == "" {
+			w.WriteError(errors.New("portfolio not provided in environment variables").Error())
+			return
+		}
+		type HTTPResponse struct {
+			Last string `json:"last"`
+		}
+
+		resp, err := http.Get("https://api.coinmarketcap.com/v1/ticker/")
+		if err != nil {
+			w.WriteError(err.Error())
+		}
+		result := &Tickers{}
+		err = json.NewDecoder(resp.Body).Decode(result)
+		if err != nil {
+			w.WriteError(err.Error())
+		}
+
+		ETHticker := result.Symbol("ETH")
+		BTCticker := result.Symbol("BTC")
+		BCHticker := result.Symbol("BCH")
+
+		if ETHticker == nil || BTCticker == nil || BCHticker == nil {
+			w.WriteError(errors.New("could not find tickers from API response").Error())
+			return
+		}
+
+		ethTotal, err := calcValue(ethAmt, ETHticker.PriceUsd)
+		if err != nil {
+			w.WriteError(errors.Wrap(err, "could not calculate total value").Error())
+
+		}
+		btcTotal, err := calcValue(btcAmt, BTCticker.PriceUsd)
+		if err != nil {
+			w.WriteError(errors.Wrap(err, "could not calculate total value").Error())
+
+		}
+		bchTotal, err := calcValue(bchAmt, BCHticker.PriceUsd)
+		if err != nil {
+			w.WriteError(errors.Wrap(err, "could not calculate total value").Error())
+
+		}
+
+		total := ethTotal + btcTotal + bchTotal
+		aud, err := usdToAud(total)
+		if err != nil {
+			w.WriteError(errors.Wrap(err, "could not convert to AUD").Error())
+
+		}
+		w.Write(fmt.Sprintf("Your crypto net worth is: %.2f USD (%.2f AUD)", total, aud))
+	}
+
+}
+
+func (cmd *PortfolioCommand) Run(cr *messagerouter.CommandRequest, rw messagerouter.ResponseWriter) error {
 	msg := strings.Fields(cr.Text)
-	err := cmd.Run(msg)
+
+	err := cmd.App.Run(msg)
 	if err != nil {
 		return err
 	}
 	return nil
+
 }
 
-func PortfolioRegister(c *cli.Context) {
-	fmt.Println("Called")
+type CLIAdaptor interface {
+	Initialise(cr *messagerouter.CommandRequest, rw messagerouter.ResponseWriter)
+	Run(cr *messagerouter.CommandRequest, rw messagerouter.ResponseWriter) error
 }
 
-// Portfolio will return a string containing the current rate in USD
-func Portfolio(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) error {
-
-	if cr.Username != "nii236" {
-		return errors.New("invalid username " + cr.Username)
+// PortfolioWrapper
+func PortfolioWrapper(app CLIAdaptor) func(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) error {
+	return func(cr *messagerouter.CommandRequest, w messagerouter.ResponseWriter) error {
+		app.Initialise(cr, w)
+		app.Run(cr, w)
+		return nil
 	}
-	ethAmt := os.Getenv("ETH")
-	btcAmt := os.Getenv("BTC")
-	bchAmt := os.Getenv("BCH")
-
-	if ethAmt == "" || btcAmt == "" || bchAmt == "" {
-		return errors.New("portfolio not provided in environment variables")
-	}
-	type HTTPResponse struct {
-		Last string `json:"last"`
-	}
-
-	resp, err := http.Get("https://api.coinmarketcap.com/v1/ticker/")
-	if err != nil {
-		return err
-	}
-	result := &Tickers{}
-	err = json.NewDecoder(resp.Body).Decode(result)
-	if err != nil {
-		return err
-	}
-
-	ETHticker := result.Symbol("ETH")
-	BTCticker := result.Symbol("BTC")
-	BCHticker := result.Symbol("BCH")
-
-	if ETHticker == nil || BTCticker == nil || BCHticker == nil {
-		return errors.New("could not find tickers from API response")
-	}
-
-	ethTotal, err := calcValue(ethAmt, ETHticker.PriceUsd)
-	if err != nil {
-		return errors.Wrap(err, "could not calculate total value")
-	}
-	btcTotal, err := calcValue(btcAmt, BTCticker.PriceUsd)
-	if err != nil {
-		return errors.Wrap(err, "could not calculate total value")
-	}
-	bchTotal, err := calcValue(bchAmt, BCHticker.PriceUsd)
-	if err != nil {
-		return errors.Wrap(err, "could not calculate total value")
-	}
-
-	total := ethTotal + btcTotal + bchTotal
-	aud, err := usdToAud(total)
-	if err != nil {
-		return errors.Wrap(err, "could not convert to AUD")
-	}
-	w.Write(fmt.Sprintf("Your crypto net worth is: %.2f USD (%.2f AUD)", total, aud))
-	return nil
 }
 
 func calcValue(ownAmt, tickerAmt string) (float64, error) {
@@ -144,5 +180,5 @@ func usdToAud(usd float64) (float64, error) {
 }
 
 func init() {
-	Handle("!portfolio", PortfolioWrapper)
+	Handle("!portfolio", PortfolioWrapper(&PortfolioCommand{}))
 }
